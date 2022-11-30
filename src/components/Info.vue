b2e88798ddc86c8b4c952fb4a13eebb9e0d91575
Erick Costa <erickcosta2509@gmail.com>
2022-11-29 23:17:01 -0300

    conhecendo as diretivas v-if, v-show

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -1,5 +1,6 @@
 <template>
-  <p>Estou estudando no momento.</p>
+  <p v-if="is_working">Estou trabalhando no momento.</p>
+  <p v-else>Estou em busca de novas oportunidades</p>
   <p>Utilizo as seguintes tecnologias:</p>
   <ul>
     <li>JavaScript</li>
@@ -7,10 +8,18 @@
     <li>React.js</li>
     <li>Vue.js</li>
   </ul>
+  <p v-show="show_email">Mande uma mensagem para: {{ email }}</p>
 </template>
 
 <script>
 export default {
   name: "Info",
+  data() {
+    return {
+      is_working: true,
+      show_email: true,
+      email: "erick@email.com",
+    }
+  },
 }
 </script>

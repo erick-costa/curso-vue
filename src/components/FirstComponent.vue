ac23304d192ee57c41e6f09f7eba5e7693a0bc5e
Erick Costa <erickcosta2509@gmail.com>
2022-11-29 22:11:54 -0300

    dados em componentes com data()

--- src/components/FirstComponent.vue
+++ src/components/FirstComponent.vue
@@ -1,9 +1,15 @@
 <template>
   <h1>Olá Vue!</h1>
+  <p>Meu nome é: {{ name }}</p>
 </template>
 
 <script>
 export default {
   name: "FirstComponent",
+  data() {
+    return {
+      name: "Erick",
+    }
+  },
 }
 </script>

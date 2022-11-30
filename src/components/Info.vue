135d0970a870079c4dc24a6b28ff0eb935671bad
Erick Costa <erickcosta2509@gmail.com>
2022-11-29 23:36:39 -0300

    atributos dinâmicos

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -9,16 +9,26 @@
     <li>Vue.js</li>
   </ul>
   <p v-show="show_email">Mande uma mensagem para: {{ email }}</p>
+  <p>
+    Para acessar meu portfólio, basta clicar
+    <a v-bind:href="my_link" target="_blank"> aqui</a>
+  </p>
+  <Picture />
 </template>
 
 <script>
+import Picture from "./Picture.vue"
 export default {
   name: "Info",
+  components: {
+    Picture,
+  },
   data() {
     return {
       is_working: true,
       show_email: true,
       email: "erick@email.com",
+      my_link: "https://github.com/erick-costa",
     }
   },
 }
---
+++ src/components/Picture.vue
@@ -0,0 +1,15 @@
+<template>
+  <img :src="avatar" :alt="alt" :title="alt" />
+</template>
+
+<script>
+export default {
+  name: "Picture",
+  data() {
+    return {
+      avatar: "/img/avatar.png",
+      alt: "Erick Costa",
+    }
+  },
+}
+</script>

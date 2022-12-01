0bdada1ac6407456f831184ae9e1d3fbfbdc17ed
Erick Costa <erickcosta2509@gmail.com>
2022-12-01 16:31:12 -0300

    eventos @submit e @click

---
+++ src/components/Form.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <h2>Meu formulário</h2>
+    <form action="" @submit="submitForm($event)">
+      <div>
+        <input type="text" v-model="name" />
+      </div>
+      <div>
+        <input type="text" v-model="email" />
+      </div>
+      <div>
+        <Submit />
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+import InputText from "./form/InputText.vue"
+import Submit from "./form/Submit.vue"
+
+export default {
+  name: "Form",
+  components: {
+    InputText,
+    Submit,
+  },
+  data() {
+    return {
+      name: "",
+      email: "",
+    }
+  },
+  methods: {
+    submitForm(e) {
+      e.preventDefault()
+      const name = this.name
+      const email = this.email
+      console.log("Formulário Enviado")
+      console.log("O nome é: " + name)
+      console.log("O email é: " + email)
+    },
+  },
+}
+</script>
--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,15 +1,18 @@
 <template>
   <h2>Essa é a descrição da pessoa: {{ name }}</h2>
   <Info />
+  <Form />
 </template>
 
 <script>
 import Info from "./Info.vue"
+import Form from "./Form.vue"
 
 export default {
   name: "Person",
   components: {
     Info,
+    Form,
   },
   data() {
     return {
